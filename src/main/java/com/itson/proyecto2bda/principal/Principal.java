2908ef8d4c52f53928d27a7ab1c7af143bc250d6
Gxboyz4 <125517545+Gxboyz4@users.noreply.github.com>
2023-03-29 16:03:35 -0700

    Paquete principal

---
+++ src/main/java/com/itson/proyecto2bda/principal/Principal.java
@@ -0,0 +1,16 @@
+/*
+Clase Principal.java creada el 29/03/2023
+*/
+package com.itson.proyecto2bda.principal;
+
+/**
+ *
+ * @author 233410_233023
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
