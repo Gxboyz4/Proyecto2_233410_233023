019a886b6ee4020a787377b19fa5cc570cc3a04c
Gabriel Mancinas <125517545+Gxboyz4@users.noreply.github.com>
2023-03-28 19:33:56 -0700

    Update Proyecto2BDA_233410_233023.java

--- src/main/java/com/mycompany/proyecto2bda_233410_233023/Proyecto2BDA_233410_233023.java
+++ src/main/java/com/mycompany/proyecto2bda_233410_233023/Proyecto2BDA_233410_233023.java
@@ -6,11 +6,11 @@ package com.mycompany.proyecto2bda_233410_233023;
 
 /**
  *
- * @author Gabriel Mancinas
+ * @author 233410_233023
  */
 public class Proyecto2BDA_233410_233023 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
