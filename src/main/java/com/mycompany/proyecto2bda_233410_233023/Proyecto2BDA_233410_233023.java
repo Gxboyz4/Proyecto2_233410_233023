1562328d88e101d21089698538f866804855cc78
Gxboyz4 <125517545+Gxboyz4@users.noreply.github.com>
2023-03-28 19:29:34 -0700

    Primer commit del proyecto

---
+++ src/main/java/com/mycompany/proyecto2bda_233410_233023/Proyecto2BDA_233410_233023.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2bda_233410_233023;
+
+/**
+ *
+ * @author Gabriel Mancinas
+ */
+public class Proyecto2BDA_233410_233023 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
