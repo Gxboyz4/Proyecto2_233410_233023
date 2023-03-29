98902054faf161b847c192b4f52697e446d4590e
Gxboyz4 <125517545+Gxboyz4@users.noreply.github.com>
2023-03-29 16:37:01 -0700

    Commit persona

---
+++ src/main/java/com/itson/proyecto2bda/dominio/Persona.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itson.proyecto2bda.dominio;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Gabriel Mancinas
+ */
+@Entity
+@Table(name="Personas")
+public class Persona implements Serializable {
+
+    public Persona() {
+    }
+    
+ 
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Persona)) {
+            return false;
+        }
+        Persona other = (Persona) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "com.itson.proyecto2bda.dominio.Persona[ id=" + id + " ]";
+    }
+    
+}
--- src/main/java/com/itson/proyecto2bda/principal/Principal.java
+++ src/main/java/com/itson/proyecto2bda/principal/Principal.java
@@ -3,6 +3,11 @@ Clase Principal.java creada el 29/03/2023
 */
 package com.itson.proyecto2bda.principal;
 
+import com.itson.proyecto2bda.dominio.Persona;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  *
  * @author 233410_233023
@@ -10,7 +15,12 @@ package com.itson.proyecto2bda.principal;
 public class Principal {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        EntityManagerFactory managerFactory = Persistence.createEntityManagerFactory("com.itson.proyecto2BDA"); //El nombre se saca de OtherSources/src/main/resources/META-INF/persistance.xml
+       EntityManager entityManager = managerFactory.createEntityManager();
+       entityManager.getTransaction().begin();
+       Persona persona = new Persona();
+       entityManager.persist(persona);
+        entityManager.getTransaction().commit();
     }
     
 }
